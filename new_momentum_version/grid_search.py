65654908b6943b5b2a1031d4dad51d36c9303978
Valerio Bonsignori <valerio12bonsignori@gmail.com>
2018-01-31 22:16:21 +0100

    solo grid_search.py l'ho rivista, ancora devo rivedere dove tagliare per il monk3. Se lanci produce graphs con la task scelta dovresti ottenere tutti i grafici. nel file sorted_names ci sono i migiori punteggi nel test

--- new_momentum_version/grid_search.py
+++ new_momentum_version/grid_search.py
@@ -5,9 +5,7 @@ import time
 
 start_time = time.time()
 
-task = "monks-3"
-early = False
-
+task = 'monks-2'
 l_train = Dataset_Loader(task + '.train')
 l_train.load_monk1()
 
@@ -18,7 +16,7 @@ n_inputs = len(l_train.x[0])
 n_outputs = len(l_train.y[0])
 nets = []
 topology = -1
-folder = 'exp_' + task + "/"
+folder = 'myexp_' + task + '/'
 
 for n_units in [5,7,10]:
     topology = topology + 1
@@ -33,84 +31,41 @@ for n_units in [5,7,10]:
 for topology in range(0,3):
     teta = -1
     for mode in ['batch','online','minibatch']:
-        for eta in [3, 5, 9]:
-            for momentum in [3, 7, 8]:
+        for eta in [.3, .5, .9]:
+            for momentum in [.3, .7, .8]:
                 if mode == 'minibatch':
                     for batch_size in [10,30,50]:
-                        if task == "monks-3":
-                            for lamb in [0.0, 0.01, 0.02]:
-                                teta = teta + 1
-                                for tries in range(0,5):
-                                    #validation_accuracy_for_topology=[]
-                                    n = pkl.load(open(folder + 'Net_'+str(topology)+'_try_'+str(tries),'rb'))
-                                    print str((n.name.split('_'))),eta,momentum,mode,lamb,(batch_size if mode =='minibatch' else '' ),str(teta)
-
-                                    loss,acc,val_loss,val_acc=n.fit(l_train.x, l_train.y, eta=eta/10., mode=mode, epochs=500, momentum=momentum/10.,batch_size=batch_size,early=early, lamb=lamb, hold_out=0.2)
-                                    predicted = []
-
-                                    for i,p in enumerate(l_test.x):
-                                        hx = n.predict(p)
-                                        predicted.append(hx)
-                                    test_acc= n.accuracy(predicted,l_test.y)
-
-                                    print 'MSE_val = ', val_loss[-1],'Validation Accuracy = ', val_acc[-1], 'Test Accuracy = ', test_acc
-
-                                    n.save_conf_and_score(folder, teta,loss,acc,val_loss,val_acc,test_acc)
-                        else:
-                            teta = teta + 1
-                            for tries in range(0, 5):
-                                # validation_accuracy_for_topology=[]
-                                n = pkl.load(open(folder + 'Net_' + str(topology) + '_try_' + str(tries), 'rb'))
-                                print str((n.name.split('_'))), eta, momentum, mode, (
-                                    batch_size if mode == 'minibatch' else ''), str(teta)
-
-                                loss, acc, val_loss, val_acc = n.fit(l_train.x, l_train.y, eta=eta / 10., mode=mode,
-                                                                     epochs=500, momentum=momentum / 10.,
-                                                                     batch_size=batch_size, early=early, hold_out=0.2)
-                                predicted = []
-
-                                for i, p in enumerate(l_test.x):
-                                    hx = n.predict(p)
-                                    predicted.append(hx)
-                                test_acc = n.accuracy(predicted, l_test.y)
-
-                                print 'MSE_val = ', val_loss[-1], 'Validation Accuracy = ', val_acc[
-                                    -1], 'Test Accuracy = ', test_acc
-
-                                n.save_conf_and_score(folder, teta, loss, acc, val_loss, val_acc, test_acc)
-                else:
-                    if task == "monks-3":
-                        for lamb in [0.0, 0.01, 0.02]:
-                            teta = teta + 1
-                            for tries in range(0,5):
-                                n = pkl.load(open(folder + 'Net_'+str(topology)+'_try_'+str(tries),'rb'))
-                                print str((n.name.split('_'))),eta,momentum,mode,lamb,(batch_size if mode =='minibatch' else '' ),str(teta)
-
-                                loss,acc,val_loss,val_acc=n.fit(l_train.x, l_train.y, eta=eta/10., mode=mode, epochs=500, momentum=momentum/10., lamb=lamb, early=early, hold_out=0.2)
-                                predicted = []
-                                for i,p in enumerate(l_test.x):
-                                    hx = n.predict(p)
-                                    predicted.append(hx)
-                                test_acc= n.accuracy(predicted,l_test.y)
-                                print 'MSE_val = ', val_loss[-1],'Validation Accuracy = ', val_acc[-1], 'Test Accuracy = ', test_acc
-                                n.save_conf_and_score(folder, teta,loss,acc,val_loss,val_acc,test_acc)
-                    else:
                         teta = teta + 1
-                        for tries in range(0, 5):
-                            n = pkl.load(open(folder + 'Net_' + str(topology) + '_try_' + str(tries), 'rb'))
-                            print str((n.name.split('_'))), eta, momentum, mode, (
-                                batch_size if mode == 'minibatch' else ''), str(teta)
+                        for tries in range(0,5):
+                            #validation_accuracy_for_topology=[]
+                            n = pkl.load(open(folder + 'Net_'+str(topology)+'_try_'+str(tries),'rb'))
+                            print str((n.name.split('_'))),eta,momentum,mode,(batch_size if mode =='minibatch' else '' ),str(teta)
 
-                            loss, acc, val_loss, val_acc = n.fit(l_train.x, l_train.y, eta=eta / 10., mode=mode,
-                                                                 epochs=500, momentum=momentum / 10., early=early,
-                                                                 hold_out=0.2)
+                            loss,acc,val_loss,val_acc=n.fit(l_train.x, l_train.y, eta=eta, mode=mode, epochs=500, momentum=momentum,batch_size=batch_size, hold_out=0.2)
                             predicted = []
-                            for i, p in enumerate(l_test.x):
+
+                            for i,p in enumerate(l_test.x):
                                 hx = n.predict(p)
                                 predicted.append(hx)
-                            test_acc = n.accuracy(predicted, l_test.y)
-                            print 'MSE_val = ', val_loss[-1], 'Validation Accuracy = ', val_acc[
-                                -1], 'Test Accuracy = ', test_acc
-                            n.save_conf_and_score(folder, teta, loss, acc, val_loss, val_acc, test_acc)
+                            test_acc= n.accuracy(predicted,l_test.y)
 
+                            print 'MSE_val = ', val_loss[-1],'Validation Accuracy = ', val_acc[-1], 'Test Accuracy = ', test_acc
+
+                            n.save_conf_and_score(folder, teta,loss,acc,val_loss,val_acc,test_acc)
+                else:
+                    teta = teta + 1
+                    for tries in range(0, 5):
+                        n = pkl.load(open(folder + 'Net_' + str(topology) + '_try_' + str(tries), 'rb'))
+                        print str((n.name.split('_'))), eta, momentum, mode, (batch_size if mode == 'minibatch' else ''), str(teta)
+
+                        loss, acc, val_loss, val_acc = n.fit(l_train.x, l_train.y, eta=eta, mode=mode,
+                                                                epochs=(200 if mode=='online' else 500), momentum=momentum,
+                                                                hold_out=0.2)
+                        predicted = []
+                        for i, p in enumerate(l_test.x):
+                            hx = n.predict(p)
+                            predicted.append(hx)
+                        test_acc = n.accuracy(predicted, l_test.y)
+                        print 'MSE_val = ', val_loss[-1], 'Validation Accuracy = ', val_acc[-1], 'Test Accuracy = ', test_acc
+                        n.save_conf_and_score(folder, teta, loss, acc, val_loss, val_acc, test_acc)
 print("--- %s seconds ---" % (time.time() - start_time))
--- new_momentum_version/produce_graphs.py
+++ new_momentum_version/produce_graphs.py
@@ -1,8 +1,42 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-folder = 'exp/'
+folder = 'exp_monks-3/'
 noplot = False
-name = 'Net_0_try_2_20.score'
+def refactor_data(topology,tries,conf):
+    #print folder+'Net_'+str(topology)+'_try_'+str(tries)+'_'+str(conf)+'.score'
+    f = open(folder+'Net_'+str(topology)+'_try_'+str(tries)+'_'+str(conf)+'.score', 'rb')
+    i = 0
+    to_write = []
+    for line in f:
+        line = line.strip()
+        if i == 0:
+            atts = line.split(' ')
+            old_atts = list(atts)
+            for i,el in enumerate(atts):
+                if el == 'n_layer:':
+                    old_atts[i]='h_units:'
+                    old_atts[i+1] = ('5' if topology ==0 else ('7' if topology==1 else '10'))
+            #print atts
+            if old_atts!=atts:
+                print atts
+                print old_atts
+                s=''
+                for el in old_atts:
+                    s=s+el+' '
+                print s
+            #to_write.append(s)
+
+            to_write.append(line)
+        else:
+            to_write.append(line)
+        i = i+1
+    f.close()
+    f = open(folder+'Net_'+str(topology)+'_try_'+str(tries)+'_'+str(conf)+'.score', 'wb')
+    i = 0
+    for line in to_write:
+        f.write(line+'\r\n')
+    f.close()
 def load_conf_and_score(topology,tries,conf):
         noplot = False
         train_conf = []
@@ -19,6 +53,7 @@ def load_conf_and_score(topology,tries,conf):
             if i==0:
                 atts = line.split(' ')
                 print atts
+                test_acc= float(atts[1])
                 for a in atts[2:]:
                     title = title + a +' '
                 #if atts[1]!='1.0':
@@ -28,34 +63,78 @@ def load_conf_and_score(topology,tries,conf):
                 train_conf = atts[2:]
             if i>1:
                 metrics = line.split('\t')
-                
+                metrics = [float(el) for el in metrics]
                 loss.append(metrics[0])
                 acc.append(metrics[1])
                 val_loss.append(metrics[2])
                 val_acc.append(metrics[3])
             i = i+1
         f.close()
-        return title,loss,acc,val_loss,val_acc
+        return title,test_acc,loss,acc,val_loss,val_acc
 
-def plot_stats(title,name,loss,acc,val_loss=[],val_acc=[],):
-        plt.axis('equal')
-        pltname = 'Net_' + name
-        c = len(loss)
-        plt.subplot(211)
-        plt.title(title)
-        plt.plot(range(0,c),acc,'k',range(0,c),val_acc,'r--')
-        plt.xlabel('ephocs')
-        plt.ylabel('Acc')
-        plt.subplot(212)
-        plt.plot(range(0,c),loss,'k',range(0,c),val_loss,'r--')
-        plt.xlabel('ephocs')
-        plt.ylabel('Loss')
-        plt.savefig(folder+pltname)
-        plt.clf()
+def plot_stats(name,loss,acc,val_loss=[],val_acc=[],color = 'light',title=''):
+    if color!='light':
+        c1, c2 = 'k', 'r'
+    else:
+        c1, c2 = 'lightgray', 'peachpuff'
+    pltname = 'Net_' + name
+    c = len(loss)
+    ax1 = plt.subplot(211)
+    if title != '':
+        plt.title(title, fontsize=20)
+    plt.plot(range(0, c), acc, linestyle='-', color=c1, label=('Acc average'if color != 'light' else ''))
+    plt.plot(range(0, c), val_acc, linestyle='--', color=c2, label=('Val_acc average'if color != 'light' else ''))
+    ax1.set_ylim(0.2,1.05)
+    #x1,x2,y1,y2 = plt.axis()
+    #plt.axis((x1,x2,0.2,1.05))
+    plt.ylabel('Acc')
+    if color!='light':
+        plt.legend(fontsize=15,loc=4)
+    ax2 = plt.subplot(212)
+    plt.plot(range(0,c),loss,linestyle='-',color=c1,label=('Loss average'if color!='light' else ''))
+    plt.plot(range(0,c),val_loss,linestyle='--',color=c2,label=('Val_loss average'if color!='light' else ''))   
+    #x1,x2,y1,y2 = plt.axis()
+    #plt.axis((x1,x2,0.4,-0.05))
+    ax2.set_ylim(.0,(0.3 if folder[-2]!='3' else 0.6))
+    plt.xlabel('ephocs')
+    plt.ylabel('Loss')
+    if color!='light':
+        plt.legend(fontsize=15,loc=1)
+    #plt.savefig(folder+str(pltname))
+    #plt.clf()
 
-for topology in range(0,3):
-    for conf in range(0,45):
-        for tries in range(0,5):    
-            confs,l,a,v_l,v_a = load_conf_and_score(topology,tries,conf)
+
+plt.figure(figsize=(10,10))
+n_tries = 5
+for topology in [0,1,2]:
+    for conf in range(0,(45 if folder[-2]!='3' else 135)):
+        confs,test_acc,l,a,v_l,v_a = load_conf_and_score(topology,0,conf)
+        test_acc_avg = np.array(test_acc)
+        l_avg = np.array(l)
+        a_avg = np.array(a)
+        v_l_avg = np.array(v_l)
+        v_a_avg = np.array(v_a)
+        if l!=[]:
+            plot_stats(str(topology)+'_'+str(0),l,a,v_l,v_a)
+        for tries in range(1,n_tries):
+            confs,test_acc,l,a,v_l,v_a = load_conf_and_score(topology,tries,conf)
+            test_acc_avg = test_acc_avg + np.array(test_acc)
+            if len(l_avg)!=len(l):
+                print len(l),len(l_avg)
+                print topology,conf
+                break #con questo break da 1_5_test-blablabla si passa a 1_7
+            l_avg = l_avg + np.array(l)
+            a_avg = a_avg + np.array(a)
+            v_l_avg = v_l_avg + np.array(v_l)
+            v_a_avg = v_a_avg + np.array(v_a)
             if l!=[]:
-                plot_stats(confs,str(topology)+'_'+str(tries),l,a,v_l,v_a)
+                plot_stats(str(topology)+'_'+str(tries),l,a,v_l,v_a)
+        test_acc_avg = test_acc_avg /float(n_tries)
+        l_avg = l_avg/float(n_tries)
+        a_avg = a_avg/float(n_tries)
+        v_l_avg = v_l_avg/float(n_tries)
+        v_a_avg = v_a_avg/float(n_tries)
+        confs = 'Test Acc average: '+ str(test_acc_avg) +'\n' + confs
+        plot_stats(str(topology)+'_'+str(tries),l_avg,a_avg,v_l_avg,v_a_avg,color='dark',title=confs)
+        plt.savefig(folder+str(topology)+'_'+str(conf)+'_Test-'+str(test_acc_avg).replace('.',','))
+        plt.clf()
